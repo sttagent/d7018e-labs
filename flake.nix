ddd717a6a71e38e67b2dcc1995c57b923eeb3ba2
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2024-04-16 11:09:25 +0200

    Update flake.lock and flake.nix files

--- flake.nix
+++ flake.nix
@@ -5,11 +5,19 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs }: 
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      name = "d7018e-labs";
+      buildInputs = with pkgs; [
+        (haskellPackages.ghcWithPackages (pkgs: with pkgs; [ 
+          haskell-language-server
+          hlint
+          QuickCheck
+        ]))
+      ];
+    };
   };
 }
