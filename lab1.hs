47070568c6b69352b8972d5741bbb1fd075c77be
Arvydas Ramanauskas <711261+sttagent@users.noreply.github.com>
2024-05-09 17:06:31 +0200

    reformat

--- lab1.hs
+++ lab1.hs
@@ -1,18 +1,17 @@
 module Lab1 where
+
 import Test.QuickCheck
-import Control.Foldl (index)
-import Data.HashMap.Internal.Array (run)
-import Test.QuickCheck.Property (Result(testCase))
+import Test.QuickCheck.Property (Result (testCase))
 
 sort :: [Int] -> [Int]
 sort [] = []
-sort (x:xs) = sort smaller ++ [x] ++ sort larger
-    where
-        smaller = [a | a <- xs, a <= x]
-        larger = [b | b <- xs, b > x]
+sort (x : xs) = sort smaller ++ [x] ++ sort larger
+  where
+    smaller = [a | a <- xs, a <= x]
+    larger = [b | b <- xs, b > x]
 
 sum' :: [Int] -> Int
-sum'  = foldl (+) 0
+sum' = foldl (+) 0
 
 max' :: [Int] -> Int
 max' = foldr max 0
@@ -22,52 +21,53 @@ min' = foldr min 0
 
 allSubLists :: [Int] -> [[Int]]
 allSubLists [] = []
-allSubLists (x:xs) = [take n (x:xs) | n <- [1..length (x:xs)]] ++ allSubLists xs
+allSubLists (x : xs) = [take n (x : xs) | n <- [1 .. length (x : xs)]] ++ allSubLists xs
 
 allIsAndJs :: [Int] -> [(Int, Int)]
-allIsAndJs xs = [(i, j) | i <- [1..length xs], j <- [i..length xs]]
+allIsAndJs xs = [(i, j) | i <- [1 .. length xs], j <- [i .. length xs]]
 
 sumSubLists :: [[Int]] -> [Int]
 sumSubLists = map sum'
 
 zipAll :: [Int] -> [(Int, Int)] -> [[Int]] -> [(Int, (Int, Int), [Int])]
 zipAll [] _ _ = []
-zipAll (x:xs) (y:ys) (z:zs) = (x, y, z) : zipAll xs ys zs
-
+zipAll (x : xs) (y : ys) (z : zs) = (x, y, z) : zipAll xs ys zs
 
 filterSubLists :: [Int] -> [(Int, (Int, Int), [Int])] -> [(Int, (Int, Int), [Int])]
 filterSubLists [] _ = []
-filterSubLists (x:xs) ys = [(y, ijs, sublist) | (y, ijs, sublist) <- ys, y == x] ++ filterSubLists xs ys
+filterSubLists (x : xs) ys = [(y, ijs, sublist) | (y, ijs, sublist) <- ys, y == x] ++ filterSubLists xs ys
 
 testList :: [Int]
 testList = [-1, 2, -3, 4, -5]
 
 allSums :: [Int] -> [Int]
-allSums xs = [min' xs..max' xs]
+allSums xs = [min' xs .. max' xs]
 
 result :: [Int] -> [(Int, (Int, Int), [Int])]
 result xs = filterSubLists (allSums xs) (zipAll sums (allIsAndJs xs) (allSubLists xs))
-    where
-        sums = sumSubLists (allSubLists xs)
+  where
+    sums = sumSubLists (allSubLists xs)
 
 printNLists :: Int -> [(Int, (Int, Int), [Int])] -> IO ()
 printNLists _ [] = return ()
 printNLists 0 _ = return ()
-printNLists n ((x, (i, j), sublist):xs) = do
-    putStrLn ("Sum: " ++ show x ++ " Indices: " ++ show i ++ " " ++ show j ++ " Sublist: " ++ show sublist)
-    printNLists (n-1) xs
-    return ()
+printNLists n ((x, (i, j), sublist) : xs) = do
+  putStrLn ("Sum: " ++ show x ++ " Indices: " ++ show i ++ " " ++ show j ++ " Sublist: " ++ show sublist)
+  printNLists (n - 1) xs
+  return ()
 
 smallestKsest :: Int -> [Int] -> IO ()
 smallestKsest k xs = printNLists k (result xs)
 
 testList1 :: [Int]
-testList1 = [x*(-1)^x | x <- [1..100]]
+testList1 = [x * (-1) ^ x | x <- [1 .. 100]]
+
 k1 :: Int
 k1 = 15
 
 testList2 :: [Int]
 testList2 = [24, -11, -34, 42, -24, 7, -19, 21]
+
 k2 :: Int
 k2 = 6
 
